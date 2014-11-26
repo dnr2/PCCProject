bfc78758a6b7012cc78dc8f2f74f2809eff9b7f1
Vitor Antero <vitor.melo@gmail.com>
2014-11-25 21:59:25 -0300

    Conserta padroes de entrada

--- src/tpf.cpp
+++ src/tpf.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <getopt.h>
 #include <stdlib.h>
+#include <fstream>
+#include <dirent.h>
+#include <sys/types.h>
 #include "tpf_algorithm.h"
 
 using namespace std;
-
-int dist = 0;
-char *pattern;
-bool patternfile = false;
-
 
 void help()
 {
@@ -16,14 +14,63 @@ void help()
 	cout << "-h, --help\t\tEste guia de ajuda" << endl;
 	cout << "-e, --edit d\t\tFaz uma busca aproximada em uma distancia maxima 'd'" << endl;
 	cout << "-p, --pattern file\tUtiliza todos os padroes em 'file' como entrada" << endl;
+	cout << "-c, --count\t\tImprime apenas a quantidade de ocorrências encontradas por arquivo" << endl;
 
 	exit(0);
+}
+
+vector<string> handle_wildcard(string &str)
+{
+	vector<string> files;
+	files.reserve(5);
+
+	// verifica se tem wildcard
+	unsigned found = str.find('*');
+	// sem wildcard
+	if (found == string::npos){
+		files.push_back(str);
+		return files;
+	}
+
+	found = str.find_last_of("/\\");
+	bool no_path = found==string::npos;
+	string path = no_path ? "./" : str.substr(0,found+1);
+	string file = no_path ? str : str.substr(found+1);
+
+  	// le arquivos do diretorio
+	DIR *dp;
+	struct dirent *ep;
+
+	dp = opendir (path.c_str());
+	if (dp != NULL)
+	{
+		while (ep = readdir (dp)){
+			//if (0 == fnmatch(file.c_str(), ep->d_name, 0)){
+				puts (ep->d_name);
+				string match(ep->d_name);
+				files.push_back(match);
+			//}
+		}
+		(void) closedir (dp);
+	}
+
+	return files;
 }
 
 int main(int argc, char **argv)
 {
 	// make COUT and CIN faster
 	ios_base::sync_with_stdio(0);cin.tie(0); 
+
+	bool edit = false;
+	bool patternfile = false;
+	bool count = false;
+
+	int error = 0;
+	vector<string> patterns;
+	string pat_file;
+	vector<string> textfiles;
+	
 	
 	int c;
 	int options = 0;
@@ -35,26 +82,30 @@ int main(int argc, char **argv)
           {"help",     no_argument,       0, 'h'},
           {"edit",     required_argument, 0, 'e'},
           {"pattern",  required_argument, 0, 'p'},
+          {"count",    no_argument, 	  0, 'c'},
           {0, 0, 0, 0}
         };
         
         int option_index = 0;
 
-        c = getopt_long (argc, argv, "he:p:", long_options, &option_index); 
+        c = getopt_long (argc, argv, "he:p:c", long_options, &option_index); 
 
         if (c == -1) break;
 
-        options += 2;
-
         switch (c){
         	case 'e':
-        		dist = atoi(optarg);
-        		cout << "distancia igual a " << dist << endl;
+        		edit = true;
+        		error = atoi(optarg);
+        		options += 2;
         		break;
         	case 'p':
         		patternfile = true;
-        		pattern = optarg;
-        		cout << "patternfile " << pattern << endl;
+        		pat_file = optarg;
+        		options += 2;
+        		break;
+        	case 'c':
+        		count = true;
+        		options += 1;
         		break;
         	case 'h':
         	case '?':
@@ -71,20 +122,51 @@ int main(int argc, char **argv)
 
 	// se um pattern file não definido, o próximo parâmetro e pattern
 	if (!patternfile){
-		pattern = argv[1];
 		options += 1;
-		cout << "pattern: " << pattern << endl;
+		string pat(argv[options]);
+		patterns.push_back(pat);
+	} else{
+		std::ifstream infile(pat_file);
+
+		if (! infile.is_open()){
+			cout << "Arquivo de padrão <<" << pat_file << ">> inválido" << endl;
+		}
+
+		string pat;
+		while (infile >> pat){
+			patterns.push_back(pat);
+		}
+	}
+
+	if (argc <= options+1){
+		cout << "Insira um ou mais arquivos de texto para realizar a busca" << endl;
+		help();
 	}
 
 	// lê os nomes dos arquivos de texto
-	cout << "Files:" << endl;
-	int textFiles = argc - options;
-	string files[textFiles];
 	for (int i = options+1; i < argc; i++){
-		files[i-options-1] = argv[i];
-		cout << files[i-options-1] << endl;
+		string str = argv[i];
+		vector<string> matches = handle_wildcard(str);
+		for(string &match : matches){
+			textfiles.push_back(match);
+		}
 	}
 
 
+	cout << "COUNT\t" << (count?"TRUE":"FALSE") << endl << endl;
+
+	cout << "EDIT\t" << (edit?"TRUE":"FALSE") << endl;
+	cout << "Value:\t" << error << endl << endl;
+
+	cout << "PATTERNFILE\t" << (patternfile?"TRUE":"FALSE") << endl;
+	cout << "PADROES:"	 << endl;
+	for(string &pat : patterns)
+		cout << pat << endl;
+	cout << endl;
+
+	cout << "ARQUIVOS DE TEXTO:" << endl;
+	for(string &file : textfiles)
+		cout << file << endl;
+	cout << endl;
 
 }
