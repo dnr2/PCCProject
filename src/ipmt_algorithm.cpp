9beb4fb9d2d082ec47dfead24a375c3adc40abd1
Vitor Antero <vitor.melo@gmail.com>
2015-02-19 22:39:23 -0300

    Adiciona as funcoes do LZ77

--- src/ipmt_algorithm.cpp
+++ src/ipmt_algorithm.cpp
@@ -3,6 +3,10 @@
 #include "SuffixArray.h"
 
 #include <assert.h>
+#include <stdlib.h>
+
+#define WINDOW_SIZE		65535
+#define BUFFER_SIZE		255
 
 using namespace std;
 
@@ -39,428 +43,85 @@ int tpf_find(vector<string> &patterns, string &textfile, int error, int tpf_type
 	return ret;
 }
 
+struct tuple{
+	int pos, size;
+	char literal;
+	tuple(int poS, int sizE, char literaL) : pos(poS), size(sizE), literal(literaL)
+	{}
+};
 
-// ---- Inicio AHO-CORASICK ---- //
-
-bool aho_corasick_search(string txt, TreeAhoCorasick & tree, vector<string> & pats, bool print_occ, vector<int> & occ) {	
-	int c = 0;	
-	bool found = false;
-	for (int i = 0; txt[i]; i++) {
-		while(c != -1 && tree.adj[c][(int)txt[i]] == -1)
-			c = tree.suf[c];
-		c = c == -1 ? 0 : tree.adj[c][(int)txt[i]];
-		int t = c;
-		while(t != -1) {
-			for (int w : tree.word_end[t]){				
-				// printf("palavra %d aparece na posicao %d\n", w, i-(tree.dep[t])+1);
-				if( print_occ ) cout << "'" << pats[w] << "', ";
-				found = true;
-				occ[w]++;
-			}
-			t = tree.preend[t];
-		}
-	}
-	return found;
+string tuple_encode(tuple tup)
+{
+	string ret = "";
+	ret += tup.pos;
+	ret += '|';
+	ret += tup.size;
+	ret += '|';
+	ret += tup.literal;
+	ret += '$';
 }
 
-int tpf_aho_corasick(vector<string> & pats, string &textfile, bool count)
+int lz77_encode(string &text, string &ret)
 {
-	
-	ifstream istream(textfile);
+	ret = "";
 
-	if (! istream.good()){
-		return TPF_ERROR_READING_FILE;
-	}
-	string line;
-	
-	//inicializar Aho-Corasick
-	TreeAhoCorasick tree;
-	tree.init();		
-	for( int i =0; i < (int) pats.size(); i++){
-		tree.add( pats[i], i );
-	}
-	
-	tree.complete_tree();	
-	
-	vector<int> occ = vector<int>( (int) pats.size());
-	
-	while ( getline(istream , line) ){
-	
-		if(!count){
-			if( aho_corasick_search( line , tree, pats, true, occ) > 0 ){
-				cout << "found at:" << endl;
-				cout << textfile << ":" << line << endl;
-			}
-		} else {
-			aho_corasick_search( line , tree, pats, false, occ);
-		}
-	}
-	if (count){
-		for( int i =0; i < (int)pats.size();i++){
-			cout << "padrao:" << pats[i] << endl;
-			cout << textfile << ":" << occ[i] << endl;
-		}
-	}
-	return TPF_OK;
-}
+	int i = 0;
 
+	string window, buffer;
 
-// ---- Fim AHO-CORASICK ---- //
+	while (i < text.length()){
+		int begin = (i > WINDOW_SIZE)? i-WINDOW_SIZE : 0;
 
-// ---- Inicio WU-MANBER ---- //
+		window = text.substr(begin, i-begin);
+		buffer = text.substr(i, BUFFER_SIZE);
 
-int char_mask(string &pat, Bitset *C)
-{
-	int m = pat.length();
+		tuple tup = tuple(0, 0, text[i]);
 
-	for (int i = 0; i < ALPHABET_SIZE; i++){
-		C[i] = Bitset(m);
-		C[i].set();
-	}
-
-	Bitset Mk(m);
-	Mk.set(0);
-
-	for (int k = 0; k < m; k++){
-		C[(int) pat[k]] = C[(int) pat[k]] & ~Mk;
-		Mk = Mk << 1;
-	}
-
-	return TPF_OK;
-}
-
-int optimized_wu_manber(string &pat, string &textfile, int error, bool count);
-
-int tpf_wu_manber(string &pat, string &textfile, int error, bool count)
-{
-	
-	if( pat.length() < 63 ){
-		return optimized_wu_manber(pat, textfile, error, count);
-	}
-	int occ = 0;
-	ifstream istream(textfile);
-
-	if (! istream.good()){
-		return TPF_ERROR_READING_FILE;
-	}
-	string line;
-
-	cout << "padrao:" << pat << endl;
-
-	
-	//inicializar bitset 
-	
-	int m = pat.length();
-	Bitset *C = new Bitset[ALPHABET_SIZE];
-	char_mask(pat, C);
-	
-	Bitset *S = new Bitset[error+1];
-	
-	// msb = 2 ** (m-1)
-	Bitset msb(m);
-	msb.set(m-1);
-	
-	Bitset S1(m);
-	Bitset S2(m);
-	
-	
-	while ( getline(istream, line) ){
-		
-		S[0] = Bitset(m);
-		S[0].set();
-
-		for (int q = 1; q <= error; q++){
-			S[q] = S[q-1] << 1;
-		}
-
-		int n = line.length();
-		bool printed = false;
-
-		for (int j = 0; j < n; j++){
-			S1 = S[0];
-			S[0] = (S[0] << 1) | C[(int) line[j]];
-			for (int q = 1; q <= error; q++){
-				S2 = S[q];
-				S[q] = 		((S[q] << 1) | C[(int) line[j]])
-						&	(S1 << 1)
-						&	(S[q-1] << 1)
-						&	S1;
-				S1 = S2;
-			}
-			//cerr  << S[error].bits[0] << " " << msb.bits[0] << endl;
-			if (S[error] < msb){
-				occ += 1;
-				
-				if (!printed && !count){
-					cout << textfile << ":" << line << endl;
-					printed = true;
+		for (int size = buffer.length(); size>0; size--){
+			int index = window.rfind(buffer.substr(0, size));
+			if (index >= 0){
+				char lit = '';
+				if (i + size < text.length()){
+					lit = text[i+size];
 				}
+				tup.pos = window.length()-index-1;
+				tup.size = size;
+				tup.literal = lit;
+				break;
 			}
 		}
+		i = i + tup.size + 1;
+		ret += tuple_encode(tup);
 	}
 
-	if (count){
-		cout << textfile << ":" << occ << endl;
-	}
-
-	return TPF_OK;
+	return 0;
 }
 
-
-
-// optimized wu mamber using long long instead of bitset
-
-typedef unsigned long long ulong;
-
-int optimized_char_mask(string &pat, ulong *C)
+int lz77_decode(string &encoded, string &text)
 {
-	int m = pat.length();
+	text = "";
+	int pos = 0;
+	int size = 0;
+	char literal;
+	while (true){
+		int idx = encoded.find('|');
 
-	for (int i = 0; i < ALPHABET_SIZE; i++){
-		C[i] = ~0ULL;
-	}
+		if (idx < 0)
+			break;
 
-	ulong Mk = 1ULL;
-
-	for (int k = 0; k < m; k++){
-		C[(int) pat[k]] = C[(int) pat[k]] & ~Mk;
-		Mk = Mk << 1;
-	}
-
-	return TPF_OK;
-}
-
-int optimized_wu_manber(string &pat, string &textfile, int error, bool count)
-{
-	
-	int occ = 0;
-	ifstream istream(textfile);
-
-	if (! istream.good()){
-		return TPF_ERROR_READING_FILE;
-	}
-	string line;
-
-	cout << "padrao:" << pat << endl;
-
-	
-	//inicializar bitset 
-	
-	int m = pat.length();
-	ulong *C = new ulong[ALPHABET_SIZE];
-	optimized_char_mask(pat, C);
-	
-	ulong *S = new ulong[error+1];
-	
-	// msb = 2 ** (m-1)
-	ulong msb = (~0ULL) << (m-1);	
-	
-	ulong S1 = 0ULL;
-	ulong S2 = 0ULL;
-	
-	
-	while ( getline(istream, line) ){
+		pos = text.length() - atoi(encoded.substr(0,idx)) - 1;
+		encoded.erase(0,idx+1);
 		
-		S[0] = ~0ULL;		
+		idx = encoded.find('|');
+		size = atoi(encoded.substr(0,idx));
+		encoded.erase(0,idx+1);
 
-		for (int q = 1; q <= error; q++){
-			S[q] = S[q-1] << 1;
-		}
+		literal = encoded[0];
+		encoded.erase(0, 2);
 
-		int n = line.length();
-		bool printed = false;
+		text += text.substr(pos, size);
+		text += literal;
 
-		for (int j = 0; j < n; j++){
-			S1 = S[0];
-			S[0] = (S[0] << 1) | C[(int) line[j]];
-			for (int q = 1; q <= error; q++){
-				S2 = S[q];
-				S[q] = 		((S[q] << 1) | C[(int) line[j]])
-						&	(S1 << 1)
-						&	(S[q-1] << 1)
-						&	S1;
-				S1 = S2;
-			}
-			//cerr << S[error] << " " << msb << endl;
-			if (S[error] < msb){
-				occ += 1;
-				
-				if (!printed && !count){
-					cout << textfile << ":" << line << endl;
-					printed = true;
-				}
-			}
-		}
 	}
-
-	if (count){
-		cout << textfile << ":" << occ << endl;
-	}
-
-	return TPF_OK;
-}
-
-// ---- Fim WU-MANBER ---- //
-
-// ---- Inicio SELLERS ---- //
-
-
-int tpf_sellers(string &pat, string &textfile, int error, bool count)
-{
-	int m = pat.length();
-	int occ = 0;
-	ifstream istream(textfile);
-
-	if (! istream.good()){
-		return TPF_ERROR_READING_FILE;
-	}
-	string line;
-
-	cout << "padrao:" << pat << endl;
-	
-	while ( getline(istream, line) ){
-		vector< vector<int> > D;		
-		
-		bool printed = false;
-	
-		int n = line.length();
-		
-		for( int i = 0; i <= m+1; i++){
-			D.push_back( vector<int>( 2 ));
-		}
-		
-		for( int i =0; i <=m; i++) 
-			D[i][0] = i;
-		for( int j =0; j < 2; j++)
-			D[0][j] = 0;
-
-		for( int j =1; j <=n; j++){
-		
-			int cur_j = j % 2; // posicao atual da matriz
-			int ant_j = cur_j ^ 1; // posicao anterior da matriz
-			
-			for( int i = 1; i<=m; i++){
-				
-				D[i][cur_j] = min( D[i-1][ant_j] + ((pat[i-1] == line[j-1])? 0 : 1), 
-					min( D[i][ant_j] + 1, D[i-1][cur_j] + 1 ));
-			}
-			if( D[m][cur_j] <= error ){
-				occ++;
-				if( !count && !printed ){
-					cout << textfile << ":" << line << endl;
-					printed = true;
-				}
-			}
-		}
-	}
-	
-	if (count){
-		cout << textfile << ":" << occ << endl;
-	}	
-	
-	return TPF_OK;
-}
-// ---- Fim SELLERS ---- //
-
-// ---- Inicio BOYER-MOORE ---- //
-
-vector<int> border(string &pat)
-{
-	int m = pat.length();
-	vector<int> B(m+1);
-	fill(B.begin()+1, B.end(), 0);
-	B[0] = -1;
-
-	int i = 1;
-
-	while (i < m){
-		int j = 0;
-		while(i+j<m && pat[i+j]==pat[j]){
-			j += 1;
-			B[i+j] = j;
-		}
-        i += j - B[j];
-	}
-
-	return B;
-}
-
-vector<int> good_suffix(string &pat)
-{
-	int m = pat.length();
-	vector<int> Pi = border(pat);
-	vector<int> PiR = border(pat);
-	vector<int> S(m+1);
-	fill(S.begin(), S.end(), m-Pi[m]);
-
-	for (int l = 1; l <= m; l++){
-		int j = m - 1 - PiR[l];
-		assert(j<m && j>=0);
-		if (pat[j]!=pat[m-l-1] && S[j] > l-PiR[l]) {
-			S[j] = l-PiR[l];
-		}
-	}
-	return S;
 
 }
-
-vector<int> bad_char(string &pat)
-{
-	int m = pat.length();
-	vector<int> C(ALPHABET_SIZE);
-	fill(C.begin(), C.end(), -1);
-
-	for (int j = 0; j < m; j++){
-		C[(int) pat[j]] = j;
-	}
-	return C;
-}
-
-int tpf_boyer_moore(string &pat, string &textfile, bool count)
-{
-	int m = pat.length();
-	vector<int> C = bad_char(pat);
-	vector<int> S = good_suffix(pat);
-	int occ = 0;
-	
-	ifstream istream(textfile);
-
-	if (! istream.good()){
-		return TPF_ERROR_READING_FILE;
-	}
-	string line;
-
-	while ( getline(istream, line) ){
-		int n = line.length();
-		int i = 0;
-		bool printed = false;
-
-		while (i <= n-m){
-			int j = m-1;
-			while (j >= 0 && line[i+j]==pat[j]){
-				j -= 1;
-			}
-			// line[i+j]==pat[j] para o padrão todo! deu match!
-			if (j < 0){
-				occ += 1;
-				i += S[0];
-				if (!printed && !count){
-					cout << textfile << ":" << line << endl;
-					printed = true;
-				}
-			}
-			else{
-				i += max(j-C[(int) line[i+j]], S[j]);
-			}
-		}
-	
-	}
-
-	if (count){
-		cout << textfile << ":" << occ << endl;
-	}
-
-	return TPF_OK;
-}
-
-// ---- Fim BOYER-MOORE ---- //
